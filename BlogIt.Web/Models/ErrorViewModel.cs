74f79e0a07bcaf7ed98eafdbf482cd5fa566f9dd
Kyle J Glover <kjglover4585@gmail.com>
2023-12-05 09:24:48 -0800

    Add project files.

---
+++ BlogIt.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BlogIt.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
