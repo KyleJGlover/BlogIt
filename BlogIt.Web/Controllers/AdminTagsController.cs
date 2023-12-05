793b315b99a08f27fe1ddf4f12e3aec6f9585445
Kyle J Glover <kjglover4585@gmail.com>
2023-12-05 12:38:02 -0800

    Added the remaind CRUD API's for the tags (Update, Delete). Added a form to handle both casses.

--- BlogIt.Web/Controllers/AdminTagsController.cs
+++ BlogIt.Web/Controllers/AdminTagsController.cs
@@ -1,4 +1,5 @@
-﻿using BlogIt.Web.Data;
+﻿using Azure;
+using BlogIt.Web.Data;
 using BlogIt.Web.Models.Domain;
 using BlogIt.Web.Models.ViewModels;
 using Microsoft.AspNetCore.Authorization;
@@ -37,7 +38,78 @@ namespace BlogIt.Web.Controllers
             blogItDbContext.Tags.Add(tag);
             blogItDbContext.SaveChanges();
 
-            return View("Add");
+            return RedirectToAction("ListAll");
+        }
+
+        [HttpGet]
+        [ActionName("ListAll")]
+        public IActionResult ListAll()
+        {
+            // use dbContext to read the tags
+            var tags = blogItDbContext.Tags.ToList();
+
+            return View(tags);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(Guid id)
+        {
+            var tag = blogItDbContext.Tags.FirstOrDefault(x => x.Id == id);
+
+            if (tag != null)
+            {
+                var editTagRequest = new EditTagRequest
+                {
+                    Id = tag.Id,
+                    Name = tag.Name,
+                    DisplayName = tag.DisplayName
+                };
+
+                return View(editTagRequest);
+            }
+
+            return View(null);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(EditTagRequest editTagRequest)
+        {
+            var tag = new Tag
+            {
+                Id = editTagRequest.Id,
+                Name = editTagRequest.Name,
+                DisplayName = editTagRequest.DisplayName
+            };
+
+            var existingTag = blogItDbContext.Tags.Find(tag.Id);
+
+            if (existingTag != null)
+            {
+                existingTag.Name = tag.Name;
+                existingTag.DisplayName = tag.DisplayName;
+                // Save Changes
+                blogItDbContext.SaveChanges();
+                return RedirectToAction("ListAll");
+            }
+
+            return RedirectToAction("Edit", new { id = editTagRequest.Id });
+        }
+
+
+        [HttpPost]
+        public IActionResult Delete(EditTagRequest editTagRequest)
+        {
+            var existingTag = blogItDbContext.Tags.Find(editTagRequest.Id);
+
+            if (existingTag != null)
+            {
+                blogItDbContext.Tags.Remove(existingTag);
+                blogItDbContext.SaveChanges();
+                return RedirectToAction("ListAll");
+            }
+
+            // Show an error notification
+            return RedirectToAction("Edit", new { id = editTagRequest.Id });
         }
     }
 }
