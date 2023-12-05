6e8b583bcd016fa0c234c8d1ac6fb7ee746f0473
Kyle J Glover <kjglover4585@gmail.com>
2023-12-05 11:54:03 -0800

    Created Add tag model to the form with data binding. Updated the Nav bar with add tags button.

---
+++ BlogIt.Web/Controllers/AdminTagsController.cs
@@ -0,0 +1,43 @@
+﻿using BlogIt.Web.Data;
+using BlogIt.Web.Models.Domain;
+using BlogIt.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace BlogIt.Web.Controllers
+{
+    //[Authorize(Roles = "Admin")]
+    public class AdminTagsController : Controller
+    {
+        private readonly BlogItDbContext blogItDbContext;
+
+        public AdminTagsController(BlogItDbContext blogItDbContext)
+        {
+            this.blogItDbContext = blogItDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+            // Map AddTagRequest to  Tag domain Model
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName,
+            };
+
+            blogItDbContext.Tags.Add(tag);
+            blogItDbContext.SaveChanges();
+
+            return View("Add");
+        }
+    }
+}
